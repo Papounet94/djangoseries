49e6df05226be4059bab9bd67845b554c875f14c
cbr <ch.brunel@free.fr>
2019-05-12 15:56:04 +0200

    Add DEBUG=False - ALLOWED_HOSTS - SECRET KEY env

--- series/settings.py
+++ series/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yl^_y06p8peh$4$fx#)w*v5!tuobv5azv_smea_p=q7ntdubl^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost',
+                 'papounet94.pythonanywhere.com',
+                 ]
 
 
 # Application definition
