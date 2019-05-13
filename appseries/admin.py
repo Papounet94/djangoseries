168a972da6ca1db556d7f4a72d5b4952e9fc94bc
cbr <ch.brunel@free.fr>
2019-05-13 18:03:19 +0200

    Add actions in admin for next week, end season, end series

--- appseries/admin.py
+++ appseries/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
 from appseries.models import Serie
 
+from datetime import timedelta, date
 
-admin.site.register(Serie)
+
+def next_week(modeladmin, request, queryset):
+    for serie in queryset:
+        serie.airing_date = serie.airing_date + timedelta(days=7)
+        serie.episode += 1
+        serie.save()
+
+
+def end_season(modeladmin, request, queryset):
+    for serie in queryset:
+        serie.airing_date = date.fromisoformat("3000-01-01")
+        serie.status = 'seasonend'
+        serie.save()
+
+
+def end_series(modeladmin, request, queryset):
+    for serie in queryset:
+        serie.airing_date = date.fromisoformat("4000-01-01")
+        serie.status = 'terminated'
+        serie.save()
+
+
+next_week.short_description = 'Next Week'
+end_season.short_description = 'End Season'
+end_series.short_description = 'End Series'
+
+
+class SerieAdmin(admin.ModelAdmin):
+    actions = [next_week, end_season, end_series, ]
+    list_display = ['title', 'airing_date', 'season', 'episode', 'status', ]
+
+
+admin.site.register(Serie, SerieAdmin)
